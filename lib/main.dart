96b918b0bf44e36bd22688a589ed9959e7ac3621
jananga99 <jananga1999@gmail.com>
2023-05-07 10:54:44 +0530

    Removed device preview added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -26,7 +28,8 @@ Future<void> main() async {
           create: (BuildContext context) => MoviesBloc(movieService)),
       BlocProvider<TvBloc>(create: (BuildContext context) => TvBloc(tvService)),
     ],
-    child: const App(),
+    child: DevicePreview(
+        enabled: !kReleaseMode, builder: (context) => const App()),
   ));
 }
 
